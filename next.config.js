136aa70f7209d869f3f63fb1884d453786b83892
Di Qi <di@racer.app>
2023-12-11 22:30:29 -0800

    Get shikiji to work

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  webpack(config) {
+    // For shikiji
+    config.module.rules.push({
+      test: /\.m?js$/,
+      type: 'javascript/auto',
+      resolve: {
+        fullySpecified: false,
+      },
+    });
+    return config;
+  },
+};
