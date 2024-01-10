58068d91627a271818f3ae79721769b2c6872231
Di Qi <di@lantern.dev>
2024-01-09 21:52:14 -0800

    Update default query

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_LONG_INPUT =
-  'I have been building web and mobile apps for 5 years, but I want to transition to full stack or backend work. I see myself as a jack of all trades. My favorite new language is Rust but I have lots of experience with Typescript.';
+  'I have been building web and mobile apps for 5 years, but I see myself as a jack of all trades. My favorite new language is Rust but I have lots of experience with Typescript. I would love a job as a founding engineer.';
 
 export const DEFAULT_SHORT_INPUT = 'React -Native';
